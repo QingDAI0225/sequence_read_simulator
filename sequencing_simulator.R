d2e65d1a5cc25819f80d5095d379b09a3232e9ff
QingDAI0225 <55598949+QingDAI0225@users.noreply.github.com>
2020-04-13 17:42:31 -0400

    Update sequencing_simulator.R

--- sequencing_simulator.R
+++ sequencing_simulator.R
@@ -1,30 +1,28 @@
 # reads simulator for nanopore sequencing
-nanopore_simulator = function(genomefile, coverage, readlength, err, read_output,align_output, error_output){
+nanopore_simulator = function(genomefile, reads, readlength, err, read_output,align_output, error_output){
     raw_data = readLines(genomefile) # load data
     genome = paste(raw_data[-1], collapse = "") # ignore the description of data in first line and transfer data into one long string
+    circlegenome = paste(genome,substr(genome, 1, readlength), sep='') # add last few fake base to imitate the circle genome
     length = nchar(genome) # get the genome length
-    read_start = sample(length, coverage*length/readlength, replace = T) # select read start position randomly
-    perfect_simulate = c() # the "perfect" reads without error
-    for (i in 1:length(read_start)){
-        perfect_simulate[i] = substr(genome, read_start[i] , read_start[i] + readlength -1)
-    }
+    read_start = sample(length, reads, replace = T) # select read start position randomly
+    perfect_simulate = sapply(read_start, function(x){substr(circlegenome, x, x+readlength-1)}) # the "perfect" reads without error
     perfect_reads = paste(perfect_simulate, collapse = "") # transfer perfect reads into one long string
     error_position = sample(nchar(perfect_reads),floor(nchar(perfect_reads)*err)) # select error position randomly
-    error_base = sample(4, floor(nchar(perfect_reads)*err), replace = T) # select error base randomly where 1 represents A, 2 represents C, 3 represents G and 4 represents T.
+    error_base = sample(3, floor(nchar(perfect_reads)*err), replace = T) # select error base randomly where we place ACGTACG in order so that the number represents the count from original base to error base.
+    replaced_base = sapply(error_position, function(x){substr(perfect_reads, x, x)})
+    replaced_number = as.numeric(gsub("A", "1", gsub("C", "2", gsub("G","3", gsub("T", "4", replaced_base)))))
     simulate = perfect_reads # add error on the perfect reads
     replaced_base = c() # record the replaced base
     for(j in 1:length(error_position)){
-        replaced_base[j] = substr(simulate, error_position[j], error_position[j])
-        substr(simulate, error_position[j], error_position[j])=as.character(error_base[j])
+        substr(simulate, error_position[j], error_position[j])= as.character(error_base[j] + replaced_number[j])
     }
-    simulate_merge = gsub("1", "A", gsub("2", "C", gsub("3","G", gsub("4", "T", simulate))))# transfer error label into base letter
-    error = gsub("1", "A", gsub("2", "C", gsub("3","G", gsub("4", "T", error_base)))) # record all error base
-    simulate_result = c() # split one long reads with error into the length we want
-    for (k in 1:(nchar(simulate_merge)/readlength)){
-        simulate_result[k] = substr(simulate_merge, (k-1)*readlength+1, k*readlength)
-    } 
+    simulate_merge = gsub("1", "A", gsub("2", "C", gsub("3","G", gsub("4", "T", gsub("5", "A", gsub("6", "C", gsub("7", "G", simulate)))))))# transfer error label into base letter
+    error = sapply(error_position, function(x){substr(simulate_merge, x, x)}) # record all error base
+    split.start = seq(1, nchar(simulate_merge), by = readlength)
+    simulate_result = sapply(split.start, function(x){substr(simulate_merge, x, x+readlength-1)}) # split one long reads with error into the length we want
+
     # build an error table to summarize the error information
-    read_number = error_position%/%readlength
+    read_number = error_position%/%readlength +1
     position = error_position%%readlength
     read_number[position[position==0]]=read_number[position[position==0]]-1
     position[position==0]=readlength
@@ -37,5 +35,5 @@ nanopore_simulator = function(genomefile, coverage, readlength, err, read_output
     write.table(simulate_result, file = read_output, sep="\t", col.names = F)
     write.table(alignment, file = align_output, sep="\t", col.name = T) 
     write.table(error_table, file = error_output, sep="\t", col.name = T) 
-    print(paste(c("results have saved as", read_output, "and", align_output)))
+    print(paste("results have saved as", read_output, align_output, ", and", error_output, sep = ' '))
 }
